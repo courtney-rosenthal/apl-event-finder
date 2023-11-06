d9db05fc4a6907dce8581b125b9d88820e690899
Courtney Rosenthal <cr@crosenthal.com>
2023-11-05 19:46:11 -0600

    work

---
+++ app-ui/build.gradle.kts
@@ -0,0 +1,18 @@
+import com.github.gradle.node.npm.task.NpmTask
+
+plugins {
+    id("com.github.node-gradle.node") version "7.0.1"
+}
+
+node {
+    download.set(true)
+    version.set("20.5.0")
+}
+
+tasks.register<NpmTask>("build") {
+    setProperty("args", listOf("run", "build"))
+}
+
+tasks.register<Exec>("clean") {
+    commandLine = listOf("rm", "-rf", "build")
+}
