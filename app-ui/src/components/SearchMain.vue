64c901a1b8b8c19715557ed7fe3ad93554feb118
Courtney Rosenthal <cr@crosenthal.com>
2023-11-10 14:08:05 -0600

    Consolidate criteria lists into criteriaSelections

--- app-ui/src/components/SearchMain.vue
+++ app-ui/src/components/SearchMain.vue
@@ -11,9 +11,32 @@ import SelectButton from 'primevue/selectbutton';
 
 import EventCard from './EventCard.vue';
 
+// Base URL of the backend API
 const BASE_URL = import.meta.env.VITE_API_BASE_URL
 
-// The search criteria that will be submitted to the API.
+
+/*
+ * Perform a call to the backend service. Catches errors, returns content body.
+ */
+async function retrieveFromBackend(url, request) {
+  try {
+    const response = await fetch(BASE_URL + url, request);
+    if (response.ok) {
+      return response.json();
+    }
+    const error = new Error(response.statusText);
+    error.json = response.json();
+    throw error;
+  } catch (error) {
+    alert("Fetch from backend service failed.\n\n" + error);
+    throw error;
+  }
+}
+
+// The search results to display.
+const searchResults = ref([])
+
+// The criteria to submit to the search API
 const searchCriteria = ref({
   days: [],
   times: [],
@@ -23,6 +46,24 @@ const searchCriteria = ref({
   searchText: null
 });
 
+/**
+ * Action performed when search is requested.
+ */
+async function performSearch() {
+  const request = {
+    method: "POST",
+    body: JSON.stringify(searchCriteria.value),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  };
+  const response = await retrieveFromBackend("/calendarEvent/search", request);
+  searchResults.value = response.results;
+}
+
+/*
+ * Action performed when search criteria reset is requested.
+ */
 function resetSearchCriteria(whut) {
   if (whut == "days" || whut == "all") {
     searchCriteria.value.days = [];
@@ -44,99 +85,74 @@ function resetSearchCriteria(whut) {
   }
 }
 
-// The search results to display.
-const searchResults = ref([])
-
-const days = [
-  {key: "MON", label: "Mon"},
-  {key: "TUE", label: "Tue"},
-  {key: "WED", label: "Wed"},
-  {key: "THU", label: "Thu"},
-  {key: "FRI", label: "Fri"},
-  {key: "SAT", label: "Sat"},
-  {key: "WEEKDAY", label: "Weekday"},
-  {key: "WEEKEND", label: "Weekend"},
-];
-
-const times = [
-  {key: "MORNING", label: "Morning"},
-  {key: "AFTERNOON", label: "Afternoon"},
-  {key: "EVENING", label: "Evening"}
-];
-
-const locations = [
-  {key: "ACB", label: "Carver"},
-  {key: "ACE", label: "Central"},
-  {key: "ACP", label: "Cepeda"},
-  {key: "AOK", label: "Hampton at Oak Hill"},
-  {key: "AHC", label: "History Center"},
-  {key: "AHO", label: "Howson"},
-  {key: "ALW", label: "Little Walnut"},
-  {key: "AMR", label: "Manchaca Road"},
-  {key: "AMI", label: "Milwood"},
-  {key: "ANV", label: "North Village"},
-  {key: "AOQ", label: "Old Quarry"},
-  {key: "APH", label: "Pleasant Hill"},
-  {key: "ARR", label: "Recycled Reads"},
-  {key: "ARZ", label: "Ruiz"},
-  {key: "ASE", label: "Southeast"},
-  {key: "ASR", label: "Spicewood Springs"},
-  {key: "ASJ", label: "St. John"},
-  {key: "ATB", label: "Terrazas"},
-  {key: "ATO", label: "Twin Oaks"},
-  {key: "AUH", label: "University Hills"},
-  {key: "AWK", label: "Willie Mae Kirk"},
-  {key: "AWP", label: "Windsor Park"},
-  {key: "AYB", label: "Yarborough"}
-];
-
-const ages = [
-  {key: "INFANT", label: "Infant (1 year and below)"},
-  {key: "TODDLER", label: "Toddler (ages 1-3)"},
-  {key: "PRESCHOOLER", label: "Preschooler (ages 3-5)"},
-  {key: "CHILD", label: "Child (ages 6-8)"},
-  {key: "PRETEEN", label: "Preteen (ages 9-11)"},
-  {key: "YOUNG_TEEN", label: "Young Teen (ages 12-14)"},
-  {key: "TEEN", label: "Teen (ages 15-17)"},
-  {key: "YOUNG_ADULT", label: "Young Adult (ages 18-21)"},
-  {key: "ADULT", label: "Adult (ages 21 and up)"},
-  {key: null, label: "All ages"}
-];
-
-
-async function performFetch(url, request) {
-  try {
-    const response = await fetch(url, request);
-    if (response.ok) {
-      return response.json();
-    }
-    const error = new Error(response.statusText);
-    error.json = response.json();
-    throw error;
-  } catch (error) {
-    alert("Fetch from backend service failed.\n\n" + error);
-    throw error;
-  }
-}
-
-async function performSearch() {
-  const request = {
-    method: "POST",
-    body: JSON.stringify(searchCriteria.value),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8"
-    }
-  };
-  const response = await performFetch(BASE_URL + "/calendarEvent/search", request);
-  searchResults.value = response.results;
-}
-
+/*
+ * On startup retrieve list of tags used.
+ */
 const allTags = ref([]);
 async function loadTags() {
-  const response = await performFetch(BASE_URL + "/calendarEvent/tags", {method: "GET"});
+  const response = await retrieveFromBackend("/calendarEvent/tags", {method: "GET"});
   allTags.value = response;
 }
 loadTags();
+
+
+/*
+ * TODO - These should be moved to the backend service, and fetched from there.
+ */
+const criteriaSelections = {
+  days: [
+    {key: "MON", label: "Mon"},
+    {key: "TUE", label: "Tue"},
+    {key: "WED", label: "Wed"},
+    {key: "THU", label: "Thu"},
+    {key: "FRI", label: "Fri"},
+    {key: "SAT", label: "Sat"},
+    {key: "WEEKDAY", label: "Weekday"},
+    {key: "WEEKEND", label: "Weekend"},
+  ],
+  times: [
+    {key: "MORNING", label: "Morning"},
+    {key: "AFTERNOON", label: "Afternoon"},
+    {key: "EVENING", label: "Evening"}
+  ],
+  locations: [
+    {key: "ACB", label: "Carver"},
+    {key: "ACE", label: "Central"},
+    {key: "ACP", label: "Cepeda"},
+    {key: "AOK", label: "Hampton at Oak Hill"},
+    {key: "AHC", label: "History Center"},
+    {key: "AHO", label: "Howson"},
+    {key: "ALW", label: "Little Walnut"},
+    {key: "AMR", label: "Manchaca Road"},
+    {key: "AMI", label: "Milwood"},
+    {key: "ANV", label: "North Village"},
+    {key: "AOQ", label: "Old Quarry"},
+    {key: "APH", label: "Pleasant Hill"},
+    {key: "ARR", label: "Recycled Reads"},
+    {key: "ARZ", label: "Ruiz"},
+    {key: "ASE", label: "Southeast"},
+    {key: "ASR", label: "Spicewood Springs"},
+    {key: "ASJ", label: "St. John"},
+    {key: "ATB", label: "Terrazas"},
+    {key: "ATO", label: "Twin Oaks"},
+    {key: "AUH", label: "University Hills"},
+    {key: "AWK", label: "Willie Mae Kirk"},
+    {key: "AWP", label: "Windsor Park"},
+    {key: "AYB", label: "Yarborough"}
+  ],
+  ages: [
+    {key: "INFANT", label: "Infant (1 year and below)"},
+    {key: "TODDLER", label: "Toddler (ages 1-3)"},
+    {key: "PRESCHOOLER", label: "Preschooler (ages 3-5)"},
+    {key: "CHILD", label: "Child (ages 6-8)"},
+    {key: "PRETEEN", label: "Preteen (ages 9-11)"},
+    {key: "YOUNG_TEEN", label: "Young Teen (ages 12-14)"},
+    {key: "TEEN", label: "Teen (ages 15-17)"},
+    {key: "YOUNG_ADULT", label: "Young Adult (ages 18-21)"},
+    {key: "ADULT", label: "Adult (ages 21 and up)"},
+    {key: null, label: "All ages"}
+  ]
+}
 
 </script>
 
@@ -144,7 +160,7 @@ loadTags();
   <Panel header="Search for ...">
 
     <Fieldset legend="Day">
-      <span v-for="day in days" :key="day.key">
+      <span v-for="day in criteriaSelections.days" :key="day.key">
         <Checkbox name="criteria.days" v-model="searchCriteria.days" :inputId="day.key" :value="day.key" /><label :for="day.key">{{ day.label }}</label>
       </span>
       <br />
@@ -152,7 +168,7 @@ loadTags();
     </Fieldset>
 
     <Fieldset legend="Time">
-      <span v-for="time in times" :key="time.key">
+      <span v-for="time in criteriaSelections.times" :key="time.key">
         <Checkbox name="criteria.times" v-model="searchCriteria.times" :inputId="time.key" :value="time.key" /><label :for="time.key">{{ time.label }}</label>
       </span>
       <br />
@@ -160,7 +176,7 @@ loadTags();
     </Fieldset>
 
     <Fieldset legend="Location">
-      <span v-for="location in locations" :key="location.key">
+      <span v-for="location in criteriaSelections.locations" :key="location.key">
         <Checkbox name="criteria.locations" v-model="searchCriteria.locations" :inputId="location.key" :value="location.key" /><label :for="location.key">{{ location.label }}</label>
       </span>
       <br />
@@ -169,7 +185,7 @@ loadTags();
 
     <Fieldset legend="Age">
       <i class="pi pi-exclamation-triangle" style="color: black; background: yellow; padding: 3px" title='Warning! The "age" criteria currently is not reliable, due to source data issues.'></i>
-      <SelectButton v-model="searchCriteria.age" :options="ages" optionLabel="label" optionValue="key" aria-labelledby="basic" />
+      <SelectButton v-model="searchCriteria.age" :options="criteriaSelections.ages" optionLabel="label" optionValue="key" aria-labelledby="basic" />
 <!--      <Button class="clear" label="clear choices" link @click="resetSearchCriteria('age')" />-->
     </Fieldset>
 
